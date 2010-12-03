65d1db43db2ef067fa09f4e3919160cee069d5d0
Jason Penney <jpenney@jczorkmid.net>
2010-12-03 17:00:59 -0500

    missed rename during refactoring

--- pymp3utils/mp3sum/__init__.py
+++ pymp3utils/mp3sum/__init__.py
@@ -1,20 +1,57 @@
+# copyright 2010 Jason Penney
+#
+# This file is part of pymp3utils
+#
+# pymp3utils is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pymp3utils is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pymp3utils. If not, see <http://www.gnu.org/licenses/>.
+
+import hashlib
 
 import eyeD3
-import hashlib
 import mp3 as MP3
-from pymp3utils import get_filepath, get_tag
 
-sum_type_default = 'sha256'
-owner_id_prefix = 'mp3sum'
+from pymp3utils import get_filename, get_tag
 
 
-def get_key(sum_type=sum_type_default):
-    return '_'.join([owner_id_prefix, sum_type])
+hash_type_default = 'sha256'
+"""
+Default hash to use if not specified in all functions that have a
+`hash_type` parameter.
+"""
+_owner_id_prefix = 'mp3sum'
 
 
-def get_sum_frame(target, sum_type=sum_type_default):
+def get_key(hash_type=hash_type_default):
+    """
+    Returns mp3sum user text frame description for given hash type.
+
+    :rtype: string
+    """
+    return '_'.join([_owner_id_prefix, hash_type])
+
+
+def get_sum_frame(target, hash_type=hash_type_default):
+    """
+    Retrieve mp3sum user text frame for given hash type.
+
+    :param target: object to act on
+    :param hash_type: hash to retrieve (optional)
+    :type hash_type: string
+    :returns: frame containing mp3sum for given hash or `None`
+    :rtype: `eyeD3.tag.UserTextFrame`
+    """
     tag = get_tag(target)
-    key = get_key(sum_type)
+    key = get_key(hash_type)
     for utf in tag.getUserTextFrames():
         if utf.description == key:
             return utf
@@ -22,43 +59,82 @@ def get_sum_frame(target, sum_type=sum_type_default):
     return None
 
 
-def read_sum(target, sum_type=sum_type_default):
-    frame = get_sum_frame(target, sum_type)
+def read_sum(target, hash_type=hash_type_default):
+    """
+     Retrieve mp3sum for given hash type.
+
+    :param target: object to act on
+    :param hash_type: hash to retrieve (optional)
+    :type hash_type: string
+    :returns: hexdigest mp3sum for given hash or `None`
+    :rtype: string
+    """
+    frame = get_sum_frame(target, hash_type)
     if isinstance(frame, eyeD3.frames.UserTextFrame):
         return frame.text
     return None
 
 
 def set_sum_frame(target, hash):
+    """
+    Store mp3sum in target
+
+    :param target: object to act on
+    :param hash: hashlib object populated with audio data frames from `target`
+    """
     tag = get_tag(target)
     key = get_key(hash.name)
     tag.addUserTextFrame(key, hash.hexdigest())
 
 
-def remove_sum_frame(target, sum_type=sum_type_default):
+def remove_sum_frame(target, hash_type=hash_type_default):
+    """
+    Remove mp3sum user text frame for given hash type, if present.
+
+    :param target: object to act on
+    :param hash_type: hash to retrieve (optional)
+    :type hash_type: string
+    """
+
     tag = get_tag(target)
-    key = get_key(sum_type)
+    key = get_key(hash_type)
     tag.addUniqueFileID(key, None)
     tag.removeUserTextFrame(key)
 
 
-def calculate_sum(target, sum_type=sum_type_default):
-    hgen = hashlib.new(sum_type)
-    f = open(get_filepath(target), "rb")
-    for hdr, frm in MP3.frames(f):
+def build_hash(target, hash_type=hash_type_default):
+    """
+    Build hash from mp3 data frames in target.
+
+    :param target: object to act on
+    :param hash_type: hash to retrieve (optional)
+    :rtype: hashlib HASH
+    """
+    hgen = hashlib.new(hash_type)
+    f = open(get_filename(target), 'rb')
+    for (hdr, frm) in MP3.frames(f):
         hgen.update(frm)
     return hgen
 
 
-def validate_file(file):
-    tag = get_tag(file)
+def validate_sum(target):
+    """
+    Validates all mp3sums found stored on target.
+    
+    :param target: object to act on
+    :rtype: list of tuples
+    :returns: hashes found in target, with validation status
+    """
+    tag = get_tag(target)
     sums = []
     results = []
     for utf in tag.getUserTextFrames():
-        if utf.description.startswith(owner_id_prefix):
-            sum_type = utf.description.split('_')[1]
-            sums.append((sum_type, utf.text))
-    for sum_type, sum_data in sums:
-        calc = calculate_sum(file, sum_type)
-        results.append((sum_type, (calc.hexdigest() == sum_data)))
+        if utf.description.startswith(_owner_id_prefix):
+            hash_type = utf.description.split('_')[1]
+            sums.append((hash_type, utf.text))
+    for (hash_type, sum_data) in sums:
+        calc = build_hash(target, hash_type)
+        results.append((hash_type, calc.hexdigest() == sum_data))
     return results
+
+
--- pymp3utils/mp3sum/console.py
+++ pymp3utils/mp3sum/console.py
@@ -1,10 +1,30 @@
-import inspect
-import argparse
+# copyright 2010 Jason Penney
+#
+# This file is part of pymp3utils
+#
+# pymp3utils is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pymp3utils is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pymp3utils. If not, see <http://www.gnu.org/licenses/>.
+
 import hashlib
+import inspect
 import sys
+
+import argparse
 from mp3 import MP3Error
+
 from pymp3utils import get_tag
-from pymp3utils.mp3sum import validate_file, sum_type_default, get_sum_frame, set_sum_frame, remove_sum_frame, calculate_sum
+from pymp3utils.mp3sum import validate_sum, hash_type_default, \
+    get_sum_frame, set_sum_frame, remove_sum_frame, build_hash
 
 
 def _validate_files(files, verbose):
@@ -12,28 +32,35 @@ def _validate_files(files, verbose):
     for f in files:
         print "'%s':" % f,
         try:
-            res = validate_file(f)
+            res = validate_sum(f)
         except MP3Error, v:
-            print "ERROR: %s" % v.args[0],
+            print 'ERROR: %s' % v.args[0],
             err += 1
             continue
         if len(res) == 0:
-            print "no checksums found",
+            print 'no checksums found',
         else:
             for r in res:
-                print "%s:" % r[0],
+                print '%s:' % r[0],
                 if r[1]:
-                    print "ok",
+                    print 'ok',
                 else:
-                    print "CORRUPT",
+                    print 'CORRUPT',
                     err += 1
-        print ""
+        print ''
 
     if err > 0:
         sys.exit(1)
 
 
-def _update_files(files, verbose, sum_types, add, remove):
+def _update_files(
+    files,
+    verbose,
+    sum_types,
+    add,
+    remove,
+    ):
+
     for f in files:
         if verbose:
             print f
@@ -43,15 +70,15 @@ def _update_files(files, verbose, sum_types, add, remove):
             if remove:
                 if get_sum_frame(tag, s) != None:
                     if verbose:
-                        print "  removing %s" % s
+                        print '  removing %s' % s
                     remove_sum_frame(tag, s)
                     update = True
             if add:
                 if get_sum_frame(tag, s) == None:
                     if verbose:
-                        print "  adding %s" % s
+                        print '  adding %s' % s
                     try:
-                        cs = calculate_sum(f, s)
+                        cs = build_hash(f, s)
                     except MP3Error, v:
                         print "ERROR in '%s': %s" % (f, v.args[0])
                         continue
@@ -60,47 +87,57 @@ def _update_files(files, verbose, sum_types, add, remove):
                     update = True
         if update:
             if verbose:
-                print " updating file"
+                print ' updating file'
             tag.update()
 
 
 def main():
+    """
+    Entrypoint for `mp3sum` command line utility
+    """
     valid_types = []
     type_check = type(hashlib.md5())
-    for t, f in inspect.getmembers(hashlib, inspect.isbuiltin):
+    for (t, f) in inspect.getmembers(hashlib, inspect.isbuiltin):
         if isinstance(f(), type_check):
             valid_types.append(t)
 
-    parser = argparse.ArgumentParser(
-        description='Manage checksums for mp3 audio data in ID3v2 tags')
+    parser = \
+        argparse.ArgumentParser(description='Manage checksums for mp3 '
+                                'audio data in ID3v2 tags')
     parser.add_argument('files', metavar='file', type=str, nargs='+',
                         help='file(s) to process')
-    parser.add_argument('-c', '--check',
-                        action='store_true',
-                        help='verify checksums (default if no other ' +
-                        'operation is requested)')
-    parser.add_argument('-u', '--update',
-                        action='store_true',
-                        help='calculate and store checksum(s) from file(s)' +
-                        ' that don\'t already have the requested checksum' +
-                        'type(s)')
-    parser.add_argument('-r', '--remove',
-                        action='store_true',
+    parser.add_argument('-c', '--check', action='store_true',
+                        help='verify checksums (default if no other '
+                        + 'operation is requested)')
+    parser.add_argument('-u', '--update', action='store_true',
+                        help='calculate and store checksum(s) from '
+                        'file(s)'
+                        + " that don't already have the requested "
+                        "checksum" + 'type(s)')
+    parser.add_argument('-r', '--remove', action='store_true',
                         help='remove stored checksum(s) from file(s)')
-    parser.add_argument('-s', '--sum-type', nargs='?', metavar='hash',
-                        default=[sum_type_default], action='append',
-                        help='type(s) of checksum(s) to calculate',
-                        choices=valid_types)
+    parser.add_argument(
+        '-s',
+        '--sum-type',
+        nargs='?',
+        metavar='hash',
+        default=[hash_type_default],
+        action='append',
+        help='type(s) of checksum(s) to calculate',
+        choices=valid_types,
+        )
     parser.add_argument('-v', '--verbose', action='store_true')
 
     args = parser.parse_args()
 
     do_check = args.check
     if args.update or args.remove:
-        _update_files(args.files, args.verbose, args.sum_type, args.update,
-                      args.remove)
+        _update_files(args.files, args.verbose, args.sum_type,
+                      args.update, args.remove)
     else:
         do_check = True
 
     if do_check:
         _validate_files(args.files, args.verbose)
+
+
