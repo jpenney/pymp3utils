cb9fe35f59322246ba97992122c34ca1a6b8fdc6
Jason Penney <jpenney@jczorkmid.net>
2010-12-22 15:49:06 -0500

    - added `v1` flag to `set_id3_version()` to add/update ID3v1 tag from ID3v2.x data - added optional `version` to `get_tag()` to load only a specific tag version - added `find_frame` - added `convert_frame`

--- pymp3utils/__init__.py
+++ pymp3utils/__init__.py
@@ -20,40 +20,40 @@ import os.path
 
 import eyeD3
 
-
 __version__ = 'unknown'
 try:
-    from pymp3utils._version import __version__
+    from pymp3utils._version import __version__ as v
+    __version__ = v
 except:
     pass
 
 
 # taken from flashbake
+
 def _find_executable(executable):
     found = filter(lambda ex: os.path.exists(ex),
-                   map(lambda path_token:
-                       os.path.join(path_token, executable),
-                       os.getenv('PATH').split(os.pathsep)))
-    if (len(found) == 0):
+                   map(lambda path_token: os.path.join(path_token,
+                   executable), os.getenv('PATH').split(os.pathsep)))
+    if len(found) == 0:
         return None
     return found[0]
 
 
 # taken from flashbake
 
-
 def _executable_available(executable):
     return _find_executable(executable) != None
 
 
-def set_id3_version(target, version):
+def set_id3_version(target, version, v1=False):
     """
     Changes the tag to the specified version if necessary.
 
     :param target: target to act on
-    :param version: tag version as defined by `eyeD3` 
+    :param version: tag version as defined by `eyeD3`
+    :param v1: flag to also save ID3v1 tags
+    :type v1: bool
     """
-
     try:
         tag = get_tag(target)
     except:
@@ -62,9 +62,11 @@ def set_id3_version(target, version):
     if tag != None:
         if tag.getVersion() != version:
             tag.update(version)
+        if v1:
+            tag.update(eyeD3.ID3_V1)
 
 
-def get_tag(target):
+def get_tag(target, version=eyeD3.ID3_ANY_VERSION):
     """
     Returns `eyeD3.Tag` for `target`.
 
@@ -72,12 +74,19 @@ def get_tag(target):
     :type target: `eyeD3.Tag`, `eyeD3.TagFile`, or string
     :rtype: `exes3.Tag`
     """
-    if isinstance(target, eyeD3.Tag):
-        return target
+
     if isinstance(target, eyeD3.TagFile):
-        return target.getTag()
+        target = target.getTag()
+
+    if isinstance(target, eyeD3.Tag):
+        if version == eyeD3.ID3_ANY_VERSION or \
+               version == target.getVersion():
+            return target
+        target = target.linkedFile
+
     if isinstance(target, str):
-        return eyeD3.Mp3AudioFile(target).getTag()
+        return eyeD3.Mp3AudioFile(target, version).getTag()
+
     raise TypeError('unable to convert to eyeD3.Tag: %s' % str(target))
 
 
@@ -107,13 +116,59 @@ def is_mp3(target):
 
     :param target: file to test
     """
+
     try:
         t = get_filename(target)
         target = t
     except:
         pass
-    try:
-        eyeD3.Mp3AudioFile(target)
-    except:
-        return False
-    return True
+    return eyeD3.isMp3File(target)
+
+
+def find_frame(target, key, search=None):
+    match = None
+    tag = get_tag(target)
+    if len(tag.frames[key]) > 0:
+        match = tag.frames[key][0]
+    if match == None and search != None:
+        for frm in tag.frames[search]:
+            if frm.description == key:
+                match = frm
+                break
+
+    return match
+
+
+def convert_frame(target, oldID, newID):
+    """
+     Convert frames of type `oldID`, or `TXXX` frames with
+     description `oldID` to frames of type `newID`
+
+     Return number of changed frames
+
+     Keyword arguments:
+     tag - the eyeD3.tag to act on
+     oldID - frame id to match
+     newID - frame id to convert to
+
+     Example usage:
+     >>> count = convertFrames(tag, 'XSOP', 'TSOP')
+
+     """
+
+    tag = get_tag(target)
+    count = 0
+    for frm in tag.frames[oldID]:
+        frm.header.id = newID
+        count = count + 1
+
+    frm = find_frame(tag, oldID, 'TXXX')
+    if frm != None:
+        frm.header.id = newID
+        frm.description = frm.text
+        frm.text = ''
+        count = count + 1
+
+    return count
+
+
